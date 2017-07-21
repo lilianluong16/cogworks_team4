f79738e32cbe36d80b207c512408da826f87a532
Jaden Tennis <jade.entling@gmail.com>
2017-07-21 09:49:45 -0400

    removed whispers

---
+++ facerec/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+def do_setup():
+    setup(name='facerec',
+          version="0.0",
+          author='Lilian Luong, Ameer Syedibrahim, Jaden Tennis',
+          description='Face database and identifying using Dlib',
+          platforms=['Windows', 'Linux', 'Mac OS-X', 'Unix'],
+          packages=find_packages())
+
+if __name__ == "__main__":
+    do_setup()
