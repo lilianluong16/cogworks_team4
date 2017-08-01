2fe3f0ec1f5d67af87e77515f9b7d2256ac5351b
Lilian Luong <lilianluong16@gmail.com>
2017-08-01 10:16:51 +0800

    GameBuddy

---
+++ gamebuddy/GameBuddy.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask_ask import Ask, question, statement
+import hangman
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+game = None
+game_state = None
+
+
+@app.route('/')
+def homepage():
+    return "GameBuddy"
+
+
+@ask.launch
+def start_skill():
+    global game
+    global game_state
+    game = None
+    game_state = None
+    return question("Hi. I'm Game Buddy. What would you like to play?")
+
+
+@ask.intent("HangmanIntent")
+def start_hangman():
+    global game
+    global game_state
+    game = "Hangman"
+    game_state = hangman.Hangman()
+    return question(game_state.start_hint())
+
+
+@ask.intent("LetterIntent")
+def get_letter(letter):
+    global game
+    if game == "Hangman":
+        if letter.lower() not in "abcdefghijklmnopqrstuvwxyz":
+            return question(letter + " is not a letter. Try again.")
+        m = game_state.guess_letter(letter.lower())
+        print(m)
+        if m[-1] == "!":
+            game = None
+            return statement(m)
+        else:
+            return question(m)
+    return start_skill()
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ gamebuddy/hangman.py
@@ -0,0 +1,54 @@
+import pickle
+import numpy as np
+
+
+class Hangman:
+    def __init__(self):
+        with open("most_common_words.txt", "rb") as f:
+            common = pickle.load(f)
+        self.common = np.array(common)
+        self.word = np.random.choice(self.common)
+        self.guess = ["_"] * len(self.word)
+        self.tries = ["head", "torso", "left arm", "right arm", "left leg", "right leg"]
+        self.letters = list("abcdefghijklmnopqrstuvwxyz")
+
+    def start_hint(self):
+        msg = "The word has " + str(len(self.word)) + " letters. What do you guess?"
+        return msg
+
+    def tell_word(self):
+        msg = "Here's what you've got so far: "
+        for c in self.guess:
+            if c == "_":
+                msg += "blank, "
+            else:
+                msg += c + ", "
+        return msg
+
+    def guess_letter(self, letter):
+        if letter in self.letters:
+            self.letters.remove(letter)
+            if letter in self.word:
+                for i in range(len(self.word)):
+                    if self.word[i] == letter:
+                        self.guess[i] = letter
+                if "_" in self.guess:
+                    return self.tell_word()
+                else:
+                    return self.end(None)
+            else:
+                part = self.tries.pop(0)
+                msg = "Oops! You've lost your " + part + ". "
+                if len(self.tries) < 1:
+                    return self.end(msg)
+                msg += self.tell_word()
+                return msg
+        else:
+            return "You've already guessed that."
+
+    def end(self, msg):
+        if msg is None:
+            m = "Congratulations! The word was " + self.word + ". Let's try again next time!"
+        else:
+            m = msg + "The word was " + self.word + ". Try again next time!"
+        return m
